d81a2ff1d4967dbb24cfe2469a374d3294499caf
KhawVicky <122266824+KhawVicky@users.noreply.github.com>
2023-11-15 19:39:06 +0800

    Update info.php

--- info.php
+++ info.php
@@ -1,6 +1,6 @@
 <?php
-    $username = isset($_POST['username']) ? $_POST['username'] : '';
-
+    $username = isset($_POST['username']) ? $_POST['username'] : ''; //get username
+    //when username == abc return verified
     if ($username === 'abc') {
         echo '<p style="color: green;">Verified</p>';
     } else {
